8cfdd63aa844a09ee7c280c90d809c535845eda6
XNishik1X <49095666+XNishik1X@users.noreply.github.com>
2021-11-25 14:05:03 -0300

    Adicionando Arquivos

---
+++ hello Github/src/br/com/joseassis/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.joseassis.git;
+
+/**
+ * @author henri
+ *Exemplo de integração eclipse/// git
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
